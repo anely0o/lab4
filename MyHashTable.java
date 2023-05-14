0b13d5d3138cd9856c07af195ac184dcf33d4cce
anely0o <ramyun.oppar@gmail.com>
2023-05-14 20:38:00 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -5,34 +5,49 @@ public class MyHashTable <K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
+
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
         @Override
         public String toString() {
             return "{" + key + " " + value + "}";
         }
     }
+
     private HashNode<K, V>[] chainArray;
     private int M = 11;
     private int size;
+
     public MyHashTable() {
         chainArray = new HashNode[M];
     }
+
     public MyHashTable(int M) {
         this.M = M;
         chainArray = new HashNode[M];
     }
+
     private int hash(K key) {
         return Math.abs(key.hashCode()) % M;
     }
+
     public void put(K key, V value) {
         int index = hash(key);
         HashNode<K, V> node = new HashNode<>(key, value);
         if (chainArray[index] == null) {
             chainArray[index] = node;
-        } else
-        }
+        } else {
+            HashNode<K, V> current = chainArray[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+            }
 
+        }
+    }
 }
