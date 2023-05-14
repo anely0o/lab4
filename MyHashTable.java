3867a38cb490ac2dce05c16f1913e1f3b02a087a
anely0o <ramyun.oppar@gmail.com>
2023-05-14 20:43:25 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -57,4 +57,12 @@ public class MyHashTable <K, V> {
         }
         size ++;
     }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> current = chainArray[index];
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
 }
