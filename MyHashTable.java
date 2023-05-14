336ea53a1b8208c0357e3b1bc42703f7c1fcbb5f
anely0o <ramyun.oppar@gmail.com>
2023-05-14 18:12:43 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -2,4 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 public class MyHashTable <K, V> {
     private static final int default_capacity = 16;
+    private List<List<Entry<K, V>>> buckets;
+    private int size;
 }
