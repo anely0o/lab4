d2c7ba3b0af48c8fd73cbad166202d2bc845553a
anely0o <ramyun.oppar@gmail.com>
2023-05-14 20:20:55 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,18 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 public class MyHashTable <K, V> {
-    private static final int default_capacity = 16;
-    private List<List<Entry<K, V>>> buckets;
-    private int size;
-    public MyHashTable() {
-        buckets = new ArrayList<>(default_capacity);
-        for (int i = 0; i < default_capacity; i++) {
-            buckets.add(new ArrayList<>());
-        }
-        size = 0;
-    }
-    public void put(K key, V value) {
-        int index = getIndex(key);
-        List<Entry<K, V>> bucket = buckets.get(index);
-    }
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
 }
