479459dd738c861c5c55d673a7e316dea2ebef6b
anely0o <ramyun.oppar@gmail.com>
2023-05-14 20:21:46 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -5,4 +5,10 @@ public class MyHashTable <K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+    }
 }
